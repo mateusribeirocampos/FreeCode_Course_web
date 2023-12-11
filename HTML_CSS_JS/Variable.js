b7da880111a6425b7cb064b4894cad1250d60932
mateusribeirocampos <mateusrcampos@gmail.com>
2023-12-11 17:51:50 -0300

    variables.js

--- HTML_CSS_JS/Variable.js
+++ HTML_CSS_JS/Variable.js
@@ -1,4 +1,15 @@
+const lastName = "Lovelace";
+let lastLetterOfLastName = lastName[lastName.length - 1];
+let firstLetterOfLastName = lastName[0];
+let secondLetterOfLastName = lastName[1];
+
+console.log(lastLetterOfLastName); 
+console.log(firstLetterOfLastName);
+console.log(secondLetterOfLastName);
+
 const someAdjective = "fun!";
 let myStr = "Learning to code is ";
 myStr += someAdjective;
 console.log(myStr); // Learning to code is
+console.log(someAdjective.length);
+console.log(myStr.length);
