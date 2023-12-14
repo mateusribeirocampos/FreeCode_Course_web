88d73e31055f448c9ffa4daf451afbcf198faa87
mateusribeirocampos <mateusrcampos@gmail.com>
2023-12-14 18:11:26 -0300

    String and array

---
+++ JavaScriptAlgorithms/Array.js
@@ -0,0 +1,5 @@
+// Url: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/store-multiple-values-in-one-variable-using-javascript-arrays
+// Name: Store Multiple Values in one Variable using JavaScript Arrays
+let myArray = ["John", "Jane", "Michael", "Emily", "David"];
+console.log(myArray);
+console.log(myArray[0]);
